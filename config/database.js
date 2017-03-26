8c99ecff7645ca42eaa7b40157a43c9b4d870c8c
Hong Jik Lee <HongJik@d-172-25-220-136.dhcp.virginia.edu>
2017-03-26 14:27:14 -0400

    final

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports ={
-	"url": "mongodb://hongjik92:zxczzxcz@olympia.modulusmongo.net:27017/wAj8ijuj"
+	"url": "mongodb://hongjik92:zxczzxcz@olympia.modulusmongo.net:27017/Radi2giv"
 
 }
