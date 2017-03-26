ad129edd93a76441cbbc03c72c6e9dcda37200df
Hong Jik Lee <HongJik@d-172-25-220-136.dhcp.virginia.edu>
2017-03-25 22:05:31 -0400

    changed the database

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports ={
-	"url": "mongodb://hongjik:zxczzxcz@jello.modulusmongo.net:27017/uHypy8wi"	
+	"url": "mongodb://hongjik92:zxczzxcz@jello.@olympia.modulusmongo.net:27017/wAj8ijuj"	
+
 }
