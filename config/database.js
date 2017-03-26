d4981725b824fd81e7b6109b482929c24b3639ce
Hong Jik Lee <HongJik@d-172-25-220-136.dhcp.virginia.edu>
2017-03-25 22:08:45 -0400

    changed database

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports ={
-	"url": "mongodb://hongjik92:zxczzxcz@@olympia.modulusmongo.net:27017/wAj8ijuj"	
+	"url": "mongodb://hongjik92:zxczzxcz@olympia.modulusmongo.net:27017/wAj8ijuj"
+
 
 
 }
